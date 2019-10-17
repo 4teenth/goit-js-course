1c2fc53a77d0d445d662fad09e7f9721c2609225
4teenth <aio.ao91@gmail.com>
2019-10-17 23:33:16 +0300

    minor upd(description)

--- goit-js-hw-02/task-03.js
+++ goit-js-hw-02/task-03.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// Напиши фукцнию findLongestWord(string), которая принимает параметром произвольную строку (в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
+
+// Вызовы функции для проверки работоспособности твоей реализации.
+
 const findLongestWord = function (string) {
   const words = string.split(' ');
   let longestWord = words[0];
